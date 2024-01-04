fb66919628eff5bbc2343459287000a39ab1a183
MarkGiddings202 <gavingiddings1997@gmail.com>
2024-01-04 00:06:39 -0500

    Updated

--- exercise-2.js
+++ exercise-2.js
@@ -12,13 +12,20 @@
 
 let numbers = [10, 20, 15, 30, 15, 20, 35, 60, 10]
 
+// "brain" storage
 let mySet = new Set()
 
+// "Looking" at each item
 for(let num of numbers){
+    // did we see this before? (check brain)
     if(mySet.has(num)){
+        // found the first duplicate!
         console.log(num)
+        //  break;
     }
+    // have not seen it, we need to store it (brain)
     mySet.add(num)
 }
 
 console.log(mySet)
+
